f72119270a24b8e22b37772667294715ededff60
Brandt2x <bfrazier11999@gmail.com>
2025-04-25 15:14:12 -0500

    Test

---
+++ Test.py
@@ -0,0 +1,16 @@
+signal = input("What color is the traffic light? ")
+
+if signal == "green":
+    print("Go")
+
+elif signal == "yellow":
+    print("Slow down")
+
+elif signal == "red":
+    print("Stop")
+
+else:
+    print("Four way stop")
+
+
+
